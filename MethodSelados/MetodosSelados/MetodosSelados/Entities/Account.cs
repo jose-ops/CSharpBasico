b0bf2ad921730728535d24865370a75c2a97fa49
J.inácio <j.inacio.gguimaraes@gmail.com>
2023-04-18 21:01:03 -0300

    methodo Selados

---
+++ MethodSelados/MetodosSelados/MetodosSelados/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpCAsting.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }//acesado somente dentro da clase ou subclass
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)//pode ser sobreEscrito ou sobre posto nas Subclasses
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;//balance sendo acesso por uma subclass 
+        }
+    }
+}
---
+++ MethodSelados/MetodosSelados/MetodosSelados/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpCAsting.Entities;
+
+namespace MetodosSelados.Entities
+{
+    class SavingsAccountPlus : SavingAccount 
+    {
+        //public override void Withdraw(double amount)//methodo selado empendido sovreescrever um mehtodo sobreescrito
+        //{
+
+        //}
+    }
+}
