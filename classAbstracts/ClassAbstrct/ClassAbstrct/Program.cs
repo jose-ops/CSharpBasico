50248431ea7edde5ac7cd85429867d2005da50eb
J.inácio <j.inacio.gguimaraes@gmail.com>
2023-05-05 22:22:28 -0300

    classe abstrata

---
+++ classAbstracts/ClassAbstrct/ClassAbstrct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            list.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
+
+            var sum = 0.0;
+            foreach (Account ass in list)
+            {
+                sum += ass.Balance;
+            }
+
+            Console.WriteLine("Total balance" + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach(Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Update balance for account" + acc.Number + ": " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
